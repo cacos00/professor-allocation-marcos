583087e94caba9af1bdae9ed34091cc94f173fad
josé marcos da silva filho <marcosfilho1095@gmail.com>
2022-08-19 22:34:53 -0300

    criação classes modelo!

---
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -0,0 +1,68 @@
+package com.project.professor.allocation.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class Professor {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String cpf;
+	
+	@Column( name= "department_id",nullable = false)
+	private Long departmentId;
+	
+	@ManyToOne
+	@JoinColumn(name= "department_id",nullable = false, insertable = false, updatable = false)
+	Department departament;
+
+	public Professor() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+}
